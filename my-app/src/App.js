9ce4c38594ce4b5d5014a7cf7720dcc802fcb8d2
Lucasnb2001 <lnb@cin.ufpe.br>
2024-06-21 16:05:03 -0300

    feat: Implementação da barra de navegação

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { TopBar } from './components/TopBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black h-screen pt-10">
+      <TopBar/>
       Hello World!
     </div>
   );
