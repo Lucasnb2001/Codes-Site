8fc78ce324f00659b5ab61ac5c5c76c8d830de78
Marcelo Bastos <marceloviniciusbs17@gmail.com>
2024-06-22 02:07:15 -0300

    feat:Parte do sobre

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Cover } from './components/Cover';
 import { TopBar } from './components/TopBar';
+import { Sobre } from './components/Sobre';
 
 function App() {
   return (
     <div className="App bg-black h-screen pt-10 text-white">
       <TopBar/>
       <Cover/>
+      <Sobre/> 
     </div>
   );
 }
